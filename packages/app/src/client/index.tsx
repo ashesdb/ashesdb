d557ce466370060d9c5e82d4a1f5db4f7b557f87
Chiel Kunkels <chiel@kunkels.me>
2021-07-18 01:42:46 +0200

    feat(app): add react-router

--- packages/app/src/client/index.tsx
+++ packages/app/src/client/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import Root from '../app/components/Root';
 
@@ -7,7 +8,7 @@ import Root from '../app/components/Root';
 	const root = document.getElementById('app-root');
 	if (!root) throw new Error('No element with id `app-root` found');
 
-	hydrate(<Root />, root, () => {
+	hydrate(<BrowserRouter><Root /></BrowserRouter>, root, () => {
 		const ssrStyles = document.getElementById('ssr-styles');
 		if (ssrStyles?.parentNode) ssrStyles.parentNode.removeChild(ssrStyles);
 	});
