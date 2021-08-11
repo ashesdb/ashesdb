a03844b495dfbc094634467ca005522aa0f47bdc
Chiel Kunkels <chiel@kunkels.me>
2021-08-12 00:05:30 +0200

    chore: set global coverage to 100

--- jest.config.ts
+++ jest.config.ts
@@ -5,16 +5,22 @@ export default {
 	coverageDirectory: 'coverage',
 	coverageThreshold: {
 		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+		"packages/app/src/client": {
 			branches: 0,
 			functions: 0,
 			lines: 0,
 			statements: 0,
 		},
-		"packages/app/src/app/utils": {
-			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
+		"packages/app/src/server": {
+			branches: 0,
+			functions: 0,
+			lines: 0,
+			statements: 0,
 		},
 	},
 	moduleNameMapper: {
