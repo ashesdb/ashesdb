1e8ca58cfb19b25066d509bbdc06b3f8a372f89d
Chiel Kunkels <chiel@kunkels.me>
2025-02-12 00:39:05 +0100

    feat(infra): create kubernetes namespace

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,14 @@
 terraform {
   required_version = ">= 1.10.1"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.48.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.35.1"
+    }
+  }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "DEPLOYER_ROLE_NAME" {
+  description = "Name of the deployer cluster role"
+  type        = string
+}
+
+variable "DIGITALOCEAN_TOKEN" {
+  description = "DigitalOcean token"
+  type        = string
+}
+
+variable "GHCR_CREDENTIALS" {
+  description = "GHCR credentials in user:token format"
+  type        = string
+}
