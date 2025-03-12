bc83f8350e72d654ac74cae3863a9c354ada2f3c
Chiel Kunkels <chiel@kunkels.me>
2025-03-12 23:08:01 +0100

    fix(skilltrees): lowercase archetype names in url

--- src/skilltrees/pages/SkillTreePicker.tsx
+++ src/skilltrees/pages/SkillTreePicker.tsx
@@ -19,7 +19,7 @@ export function SkillTreePicker() {
 				<ArchetypeCard
 					key={archetype.name}
 					archetype={archetype}
-					href={`/archetype/${archetype.name}`}
+					href={`/archetype/${archetype.name.toLowerCase()}`}
 				/>
 			))}
 		</div>
