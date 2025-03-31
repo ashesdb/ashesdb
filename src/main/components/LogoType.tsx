8b0b98568e53a4632fdaf75a8c16df74a0038b7f
Chiel Kunkels <chiel@kunkels.me>
2025-03-31 22:21:18 +0200

    feat(nav): link logo back to home

--- src/main/components/LogoType.tsx
+++ src/main/components/LogoType.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'wouter';
+
 import css from './LogoType.module.css';
 
 export function LogoType() {
 	return (
 		<h1 className={css.logo}>
-			Ashes<span>DB</span>
+			<Link className={css.link} href="/">
+				Ashes<span>DB</span>
+			</Link>
 		</h1>
 	);
 }
